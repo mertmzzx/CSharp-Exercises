7213816fcf9d0ed0bdae2872a88f0768344d3017
mertmzzx <wubster@abv.bg>
2022-05-19 19:25:51 +0300

    Added Multidimensional Arrays Exercise

---
+++ C# Advanced/Multidimensional Arrays - Exercise/T02. Squares in Matrix/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace T02._Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int rows = int.Parse(input.Split()[0]);
+            int cols = int.Parse(input.Split()[1]);
+
+            string[,] matrix = new string[rows, cols];
+            FillMatrix(matrix);
+            int count = 0;
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] 
+                        && matrix[row, col] == matrix[row + 1, col] 
+                        && matrix[row, col] == matrix[row + 1, col + 1]) 
+                    {
+                        count++;
+                    }
+
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+
+        private static void FillMatrix(string[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] rowData = Console.ReadLine().Split(); 
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+        }
+    }
+}
