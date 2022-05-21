65fb90a88846be1448aa9fa920dfe88190178512
mertmzzx <wubster@abv.bg>
2022-05-21 15:33:37 +0300

    Updated Multidimensional Arrays - Lab

--- C# Advanced/Multidimensional Arrays/03. Primary Diagonal/Program.cs
+++ C# Advanced/Multidimensional Arrays/03. Primary Diagonal/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _03._Primary_Diagonal
 {
@@ -6,7 +7,39 @@ namespace _03._Primary_Diagonal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            FillMatrix(matrix);
+
+            int sumPrimaryDiagonal = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (row == col)
+                    {
+                        sumPrimaryDiagonal += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine(sumPrimaryDiagonal);
+
+        }
+
+        private static void FillMatrix(int[,] numbers)
+        {
+            for (int row = 0; row < numbers.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int col = 0; col < numbers.GetLength(1); col++)
+                {
+                    numbers[row, col] = rowData[col];
+                }
+            }
         }
     }
 }
--- C# Advanced/Multidimensional Arrays/04. Symbol in Matrix/Program.cs
+++ C# Advanced/Multidimensional Arrays/04. Symbol in Matrix/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _04._Symbol_in_Matrix
 {
@@ -6,7 +7,45 @@ namespace _04._Symbol_in_Matrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] symbols = new char[n, n];
+            FillMatrix(symbols);
+
+            bool isFound = false;
+
+            char ch = char.Parse(Console.ReadLine());
+            int symbolInInteger = ch;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (symbols[row, col] == ch)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        isFound = true;
+                        return;
+                    }
+                }
+            }
+
+            if (isFound == false)
+            {
+                Console.WriteLine($"{ch} does not occur in the matrix");
+            }
+        }
+
+        private static void FillMatrix(char[,] symbols)
+        {
+            for (int row = 0; row < symbols.GetLength(0); row++)
+            {
+                char[] rowData = Console.ReadLine().ToArray();
+                for (int col = 0; col < symbols.GetLength(1); col++)
+                {
+                    symbols[row, col] = rowData[col];
+                }
+            }
         }
     }
 }
