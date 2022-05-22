d19bd7a24f3dc8912fda4b359930326e7d717370
mertmzzx <wubster@abv.bg>
2022-05-22 18:14:50 +0300

    Updated Exercises

--- C# Advanced/Multidimensional Arrays - Exercise/T03. Maximal Sum/Program.cs
+++ C# Advanced/Multidimensional Arrays - Exercise/T03. Maximal Sum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace T03._Maximal_Sum
 {
@@ -6,7 +7,63 @@ namespace T03._Maximal_Sum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] n = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = n[0];
+            int cols = n[1];
+            int[,] matrix = new int[rows, cols];
+
+            FillMatrix(matrix);
+            int Sum = 0;
+            int maxRow = 0;
+            int maxCol = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (row + 2 < rows && col + 2 < cols)
+                    {
+                        int currSum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2] +
+                                      matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2] +
+                                      matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+
+                        if (currSum > Sum)
+                        {
+                            Sum = currSum;
+                            maxRow = row;
+                            maxCol = col;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum = {Sum}");
+
+            for (int row = maxRow; row < maxRow + 3; row++)
+            {
+                for (int col = maxCol; col < maxCol + 3; col++)
+                {
+                    Console.Write(matrix[row,col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void FillMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
         }
     }
 }
--- C# Advanced/Multidimensional Arrays - Exercise/T07. Knight Game/Program.cs
+++ C# Advanced/Multidimensional Arrays - Exercise/T07. Knight Game/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace T07._Knight_Game
 {
@@ -6,7 +7,103 @@ namespace T07._Knight_Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int size = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[size, size];
+            FillMatrix(matrix);
+
+            int knight = 0;
+
+            while (true)
+            {
+                int rowIndexKnight = -1;
+                int colIndexKnight = -1;
+
+                int attacks = 0;
+
+                for (int row = 0; row < size; row++)
+                {
+                    for (int col = 0; col < size; col++)
+                    {
+                        if (matrix[row, col] == 'K')
+                        {
+                            int currAtt = CurrentAttack(matrix, row, col);
+
+                            if (currAtt > attacks)
+                            {
+                                attacks = currAtt;
+                                rowIndexKnight = row;
+                                colIndexKnight = col;
+                            }
+                        }
+                    }
+                }
+
+                if (attacks > 0)
+                {
+                    matrix[rowIndexKnight, colIndexKnight] = '0';
+                    knight++;
+                }
+                else break;
+            }
+
+            Console.WriteLine(knight);
+        }
+
+        private static int CurrentAttack(char[,] matrix, int row, int col)
+        {
+            int numberAttacks = 0;
+
+            if (IsValid(row - 1, col - 2, matrix) && matrix[row - 1, col - 2] == 'K')
+            {
+                numberAttacks++;
+            }
+            if (IsValid(row - 1, col + 2, matrix) && matrix[row - 1, col + 2] == 'K')
+            {
+                numberAttacks++;
+            }
+            if (IsValid(row + 1, col - 2, matrix) && matrix[row + 1, col - 2] == 'K')
+            {
+                numberAttacks++;
+            }
+            if (IsValid(row + 1, col + 2, matrix) && matrix[row + 1, col + 2] == 'K')
+            {
+                numberAttacks++;
+            }
+            if (IsValid(row - 2, col - 1, matrix) && matrix[row - 2, col - 1] == 'K')
+            {
+                numberAttacks++;
+            }
+            if (IsValid(row - 2, col + 1, matrix) && matrix[row - 2, col + 1] == 'K')
+            {
+                numberAttacks++;
+            }
+            if (IsValid(row + 2, col - 1, matrix) && matrix[row + 2, col - 1] == 'K')
+            {
+                numberAttacks++;
+            }
+            if (IsValid(row + 2, col + 1, matrix) && matrix[row + 2, col + 1] == 'K')
+            {
+                numberAttacks++;
+            }
+
+            return numberAttacks;
+        }
+
+        private static void FillMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] rowData = Console.ReadLine().ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+        }
+
+        private static bool IsValid(int row, int col, char[,] matrix)
+        {
+            return row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1);
         }
     }
 }
