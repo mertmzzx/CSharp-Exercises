e00ce65c5df6b973a61a39288e0069726b6863f8
mertmzzx <wubster@abv.bg>
2022-05-24 16:09:17 +0300

    Updated Sets and Dictionaries Advanced

---
+++ C# Advanced/Sets and Dictionaries Advanced/07. Parking Lot/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> cars = new HashSet<string>();
+            string cmd = Console.ReadLine();
+
+            while (cmd != "END")
+            {
+                string direction = cmd.Split(", ")[0];
+                string carNumber = cmd.Split(", ")[1];
+
+                if (direction == "IN")
+                {
+                    if (!cars.Contains(carNumber))
+                    {
+                        cars.Add(carNumber);
+                    }
+                }
+                else
+                {
+                    if (cars.Contains(carNumber))
+                    {
+                        cars.Remove(carNumber);
+                    }   
+                }
+
+                cmd = Console.ReadLine();
+            }
+
+            if (cars.Count > 0 )
+            {
+                foreach (var car in cars)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+
+        }
+    }
+}
---
+++ C# Advanced/Sets and Dictionaries Advanced/08. SoftUni Party/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._SoftUni_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> regular = new HashSet<string>();
+            HashSet<string> vip = new HashSet<string>();
+
+            string people = Console.ReadLine();
+            AddGuests(vip, regular, people);
+
+            people = Console.ReadLine();
+            ClearGuests(vip, regular, people);
+
+            int count = vip.Count + regular.Count;
+            Console.WriteLine(count);
+
+            if (vip.Count > 0)
+            {
+                foreach (var guest in vip)
+                {
+                    Console.WriteLine(guest);
+                }
+            }
+            if (regular.Count > 0)
+            {
+                foreach (var guest in regular)
+                {
+                    Console.WriteLine(guest);
+                }
+            }
+        }
+
+        static void ClearGuests(HashSet<string> vip, HashSet<string> regular, string people)
+        {
+            while (people != "END")
+            {
+                if (char.IsDigit(people[0]))
+                {
+                    if (vip.Contains(people))
+                    {
+                        vip.Remove(people);
+                    }
+                }
+                else
+                {
+                    if (regular.Contains(people))
+                    {
+                        regular.Remove(people);
+                    }
+                }
+
+                people = Console.ReadLine();
+            }
+        }
+
+        static void AddGuests(HashSet<string> vip, HashSet<string> regular, string people)
+        {
+            while (people != "PARTY")
+            {
+                if (char.IsDigit(people[0]))
+                {
+                    if (!vip.Contains(people))
+                    {
+                        vip.Add(people);
+                    }
+                }
+                else
+                {
+                    if (!regular.Contains(people))
+                    {
+                        regular.Add(people);
+                    }
+                }
+
+                people = Console.ReadLine();
+            }
+        }
+    }
+}
