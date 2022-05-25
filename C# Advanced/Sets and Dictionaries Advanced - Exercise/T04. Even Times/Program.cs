7cd1a23e17ca53e9924eea056a45aa6256ddcf81
mertmzzx <wubster@abv.bg>
2022-05-25 18:10:30 +0300

    Added Sets and Dictionaries Advanced - Exercise

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/T04. Even Times/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T04._Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/T06. Wardrobe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T06._Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
