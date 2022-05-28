e51c9d24b70d70dbe82201fd836ec35199b569a1
mertmzzx <wubster@abv.bg>
2022-05-28 13:13:39 +0300

    Added Streams, Files and Directories - Exercise

---
+++ C# Advanced/Streams, Files and Directories - Exercise/EvenLines/EvenLines.cs
@@ -0,0 +1,54 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            ProcessLines(inputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                int counter = -1; //брой редовете
+                string line = reader.ReadLine(); //четем по 1 ред от файла
+
+                while (line != null)
+                {
+                    counter++;
+                    if (counter % 2 == 0)
+                    {
+                        //замяна
+                        line = Replace(line);
+                        //обърна в обратен ред
+                        line = Reverse(line);
+                        Console.WriteLine(line);
+                    }
+
+                    line = reader.ReadLine();
+                }
+            }
+        }
+
+        private static string Reverse(string line)
+        {
+            return string.Join(" ", line.Split().Reverse());
+        }
+
+        private static string Replace(string line) //върне реда със заместени символи
+        {
+            return line.Replace("-", "@")
+                 .Replace(",", "@")
+                 .Replace(".", "@")
+                 .Replace("!", "@")
+                 .Replace("?", "@");
+        }
+    }
+}
---
+++ C# Advanced/Streams, Files and Directories - Exercise/LineNumbers/LineNumbers.cs
@@ -0,0 +1,40 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt"; // файл за четене
+            string outputFilePath = @"..\..\..\output.txt"; //файл за писане
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+            int count = 0;
+            List<string> outputLines = new List<string>();
+
+            foreach (string line in lines)
+            {
+                count++;
+                //2. броим буквите
+                int countLetters = line.Count(char.IsLetter); //37
+                //3. броим символите, които са пунктуационни знаци
+                int countSymbol = line.Count(char.IsPunctuation); //4
+                //1. номер на реда
+                string modifiedLine = $"Line {count}: {line} ({countLetters})({countSymbol})";
+
+                outputLines.Add(modifiedLine);
+            }
+
+            File.WriteAllLines(outputFilePath, outputLines);
+        }
+    }
+}
