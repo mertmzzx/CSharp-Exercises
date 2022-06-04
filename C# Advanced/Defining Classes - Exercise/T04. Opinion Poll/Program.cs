48d8603956c3165e1c3a0fb66b62d09a47aa3af7
mertmzzx <wubster@abv.bg>
2022-06-04 13:12:29 +0300

    Added Defining Classes - Exercise

---
+++ C# Advanced/Defining Classes - Exercise/T04. Opinion Poll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T04._Opinion_Poll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
