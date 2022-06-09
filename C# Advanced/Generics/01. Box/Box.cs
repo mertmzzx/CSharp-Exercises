dd7d3ca3089f0622b4f7449bf29e78522ab89cf6
mertmzzx <wubster@abv.bg>
2022-06-09 18:02:14 +0300

    Added Lab

---
+++ C# Advanced/Generics/01. Box/Box.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+            this.BoxCollection = new List<T>();
+        }
+        public void Add(T element)
+        {
+            BoxCollection.Add(element);
+        }
+
+        public T Remove()
+        {
+            var element = this.BoxCollection[Count - 1];
+            BoxCollection.Remove(BoxCollection[Count - 1]);
+            return element;
+        }
+
+        public List<T> BoxCollection { get; set; }
+        int Count { get { return BoxCollection.Count; } }
+    }
+}
---
+++ C# Advanced/Generics/02. Array Creator/ArrayCreator.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+        public static T[] Create<T>(int lenght, T item)
+        {
+            T[] array = new T[lenght];
+            for (int i = 0; i < lenght; i++)
+            {
+                array[i] = item;
+            }
+
+            return array;
+        }
+    }
+}
