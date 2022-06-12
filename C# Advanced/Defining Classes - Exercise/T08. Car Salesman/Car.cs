e0e447a26a3197583c000a5e1a65cdaeec5437cc
mertmzzx <wubster@abv.bg>
2022-06-12 13:11:30 +0300

    Updated task

--- C# Advanced/Defining Classes - Exercise/T08. Car Salesman/Car.cs
+++ C# Advanced/Defining Classes - Exercise/T08. Car Salesman/Car.cs
@@ -1,4 +1,6 @@
-﻿namespace DefiningClasses
+﻿using System.Text;
+
+namespace DefiningClasses
 {
     public class Car
     {
@@ -14,5 +16,19 @@
        public int Weight { get; set; }
        public string Color { get; set; }
 
+       public override string ToString()
+       {
+           StringBuilder sb = new StringBuilder();
+
+           sb.AppendLine($"{Model}:");
+           sb.AppendLine($"  {Engine.Model}:");
+           sb.AppendLine($"    Power: {Engine.Power}");
+           sb.AppendLine($"    Displacement: {(Engine.Displacement != 0 ? Engine.Displacement.ToString() : "n/a")}");
+           sb.AppendLine($"    Efficiency: {Engine.Efficiency ?? "n/a"}");
+           sb.AppendLine($"  Weight: {(Weight != 0 ? Weight.ToString() : "n/a")}");
+           sb.AppendLine($"  Color: {Color ?? "n/a"}");
+
+           return sb.ToString().TrimEnd();
+       }
     }
 }
---
+++ C# Advanced/Defining Classes - Exercise/T08. Car Salesman/StartUp.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Engine> engines = new List<Engine>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string model = data[0];
+                int power = int.Parse(data[1]);
+
+                Engine engine = new Engine(model, power);
+
+                if (data.Length == 3)
+                {
+                    if (int.TryParse(data[2], out int displacement))
+                    {
+                        engine.Displacement = displacement;
+                    }
+                    else
+                    {
+                        engine.Efficiency = data[2];
+                    }
+                }
+                else if (data.Length == 4)
+                {
+                    int displacement = int.Parse(data[2]);
+                    string eff = data[3];
+
+                    engine.Displacement = displacement;
+                    engine.Efficiency = eff;
+                }
+
+                engines.Add(engine);
+            }
+
+            List<Car> cars = new List<Car>();
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string model = data[0];
+
+                Engine engine = engines.First(x => x.Model == data[1]);
+                Car car = new Car(model, engine);
+
+                if (data.Length == 3)
+                {
+                    if (int.TryParse(data[2], out int weight))
+                    {
+                        car.Weight = weight;
+                    }
+                    else
+                    {
+                        car.Color = data[2];
+                    }
+                }
+                else if (data.Length == 4)
+                {
+                    int weight = int.Parse(data[2]);
+                    string color = data[3];
+
+                    car.Weight = weight;
+                    car.Color = color;
+                }
+
+                cars.Add(car);
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
