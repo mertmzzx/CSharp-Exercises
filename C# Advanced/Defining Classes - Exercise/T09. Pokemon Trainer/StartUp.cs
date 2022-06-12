30bc9a570c78c631588ba203c1d6c4ab2937075d
mertmzzx <wubster@abv.bg>
2022-06-12 14:38:58 +0300

    Updated task to 60/100

---
+++ C# Advanced/Defining Classes - Exercise/T09. Pokemon Trainer/StartUp.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Trainer> trainers = new List<Trainer>();
+            string data;
+
+            while ((data = Console.ReadLine()) != "Tournament")
+            {
+                string[] tokens = data.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string trainerName = tokens[0];
+                string pokemonName = tokens[1];
+                string pokemonEl = tokens[2];
+                int pokemonHealth = int.Parse(tokens[3]);
+
+                Pokemon pokemon = new Pokemon(pokemonName, pokemonEl, pokemonHealth);
+                Trainer trainer = trainers.FirstOrDefault(x => x.Name == trainerName);
+
+                if (trainer == null)
+                {
+                    trainers.Add(new Trainer(trainerName, pokemon));
+                }
+                else
+                {
+                    trainer.pokemonCollection.Add(pokemon);
+                }
+            }
+
+            while ((data = Console.ReadLine()) != "End")
+            {
+                foreach (var trainer in trainers)
+                {
+                    if (trainer.pokemonCollection.FirstOrDefault(x => x.Element == data) != null)
+                    {
+                        trainer.Badges++;
+                    }
+                    else
+                    {
+                        RemoveOrUpdate(trainer);
+                    }
+                }
+            }
+
+            foreach (var trainer in trainers.OrderByDescending(x => x.Badges))
+            {
+                Console.WriteLine(trainer);
+            }
+        }
+
+        static void RemoveOrUpdate(Trainer trainer)
+        {
+            foreach (var pokemon in trainer.pokemonCollection)
+            {
+                pokemon.Health -= 10;
+
+                if (pokemon.Health <= 0)
+                {
+                    trainer.pokemonCollection.Remove(pokemon);
+                }
+
+                if (trainer.pokemonCollection.Count == 0)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes - Exercise/T09. Pokemon Trainer/Trainer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class Trainer
+    {
+        
+
+        public Trainer(string name, Pokemon pokemon)
+        {
+            this.Name = name;
+            this.Badges = 0;
+            this.pokemonCollection.Add(pokemon);
+        }
+
+        public string Name { get; set; }
+        public int Badges { get; set; }
+        public List<Pokemon> pokemonCollection { get; set; } = new List<Pokemon>();
+
+        public override string ToString()
+        {
+            return $"{Name} {Badges} {pokemonCollection.Count}";
+        }
+    }
+}
