79570449e1ad9d72730eb935ff68f00998b19302
mertmzzx <wubster@abv.bg>
2022-06-15 17:31:18 +0300

    Added Exercises

---
+++ C# Advanced/Iterators and Comparators/Iterators and Comparators - Exercise/T02. Collection/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Collection
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command = "";
+            ListyIterator<string> listy = null;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                var tokens = command.Split();
+
+                if (tokens[0] == "Create")
+                {
+                    listy = new ListyIterator<string>(tokens.Skip(1).ToArray());
+                }
+                else if (tokens[0] == "Move")
+                {
+                    Console.WriteLine(listy.Move());
+                }
+                else if (tokens[0] == "Print")
+                {
+                    listy.Print();
+                }
+                else if (tokens[0] == "HasNext")
+                {
+                    Console.WriteLine(listy.HasNext());
+                }
+            }
+        }
+    }
+}
