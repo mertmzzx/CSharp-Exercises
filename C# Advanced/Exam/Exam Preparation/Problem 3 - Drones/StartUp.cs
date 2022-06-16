2d649d108b2f4f2b51e53785b94feef3b75e184d
mertmzzx <wubster@abv.bg>
2022-06-16 18:10:42 +0300

    Added Exam Preparation

---
+++ C# Advanced/Exam/Exam Preparation/Problem 3 - Drones/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Drones
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Airfield airfield = new Airfield("Heathrow", 10, 10.5);
+            Drone drone = new Drone("D20", "DEERC", 6);
+
+
+            Console.WriteLine(drone);
+
+            Console.WriteLine(airfield.AddDrone(drone));
+
+            Console.WriteLine(airfield.Count);
+
+            Console.WriteLine(airfield.Report());
+        }
+    }
+}
