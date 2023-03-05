91700913a58d02ad7d57e8c634ef341f099b4f5d
Mert <wubster@abv.bg>
2023-03-05 22:13:27 +0200

    Added LINQ Exercises

---
+++ C# DB/Entity Framework Core/LINQ/MusicHub/Data/ValidationConstants.cs
@@ -0,0 +1,26 @@
+﻿namespace MusicHub.Data;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class ValidationConstants
+{
+    //Song
+    public const int SongNameMaxLenght = 20;
+
+    //Album
+    public const int AlbumNameMaxLength = 40;
+
+    //Performer
+    public const int PerformerFirstNameMaxLength = 20;
+    public const int PerformerLastNameMaxLength = 20;
+
+    //Producer
+    public const int ProducerNameMaxLength = 30;
+
+    public const int WriterNameMaxLength = 20;
+}
+
